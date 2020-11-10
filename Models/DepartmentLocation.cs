98e0086ea0b20f08eab721e8eab871767c18f97e
Michelle Vanwettere <michellevanwettere@msn.com>
2020-11-10 10:58:56 -0500

    Add project files.

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        //Composite Primary Key and foreign key for department
+        public int DepartmentID { get; set; }
+        //Composite Primary Key and foreign key for location
+        public int LocationID { get; set; }
+
+        // navigation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
